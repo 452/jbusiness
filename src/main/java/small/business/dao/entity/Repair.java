34c616015c9cb4ec1b10ce09112a4d2d52ab2cf7
emetemunoy <sp.titan@gmail.com>
2012-09-14 20:22:33 +0200

    update todo and add repair entity

---
+++ src/main/java/small/business/dao/entity/Repair.java
@@ -0,0 +1,18 @@
+package small.business.dao.entity;
+
+import java.io.Serializable;
+
+public class Repair implements Serializable {
+
+	private static final long	serialVersionUID	= 1L;
+	
+	private Long id;
+	private String dateOfAdmissionToRepair;
+	private String dateOfBeginRepair;
+	private String dateOfEndRepair;
+	private String dateOfTakenFromRepair;
+	private CounterParties counterParty;
+	private double price;
+	private String info;
+
+}
